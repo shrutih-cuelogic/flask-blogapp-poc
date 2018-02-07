4f5d09512bad7b1518a7f2c36c33906f36ecde3c
shrutih-cuelogic <shruti.hiremath@cuelogic.co.in>
2018-02-07 20:10:45 +0530

    dchanged mysql to postgre and dockerised the app

---
+++ config.py
@@ -0,0 +1,44 @@
+import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+class Auth:
+    CLIENT_ID = ('833108435253-hs7dmgaerscr01tqr75d14rmu81li6rc.apps.googleusercontent.com')
+    CLIENT_SECRET = 'MSJ4Q_YrBCE4qcWZgv8G-Aoq'
+    REDIRECT_URI = 'http://localhost:8080/oauth2callback'
+    AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
+    TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
+    USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
+    SCOPE = [
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://mail.google.com/',
+        'https://www.googleapis.com/auth/gmail.compose',
+        'https://www.googleapis.com/auth/gmail.send',
+        'https://www.googleapis.com/auth/gmail.insert',
+        'https://www.googleapis.com/auth/gmail.labels',
+        'https://www.googleapis.com/auth/gmail.modify',
+        'https://www.googleapis.com/auth/plus.login',
+        'https://www.googleapis.com/auth/gmail.modify',
+        'https://www.googleapis.com/auth/gmail.settings.sharing',
+        'https://www.googleapis.com/auth/gmail.settings.basic'
+    ]
+
+
+class Config(object):
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql://flask_user:password@localhost/flask_blog'
+    SECRET_KEY = 'b240ac48e41e84a4278d195092289a8bdb08556b22f6760d'
+    SQLALCHEMY_TRACK_MODIFICATIONS =True
+    # FIXTURE_DIRS = (
+    #    'app/auth/fixtures/',
+    #    'app/blog/fixtures/'
+    # )
+
+class TestConfig(Config):
+    """docstring for TestConfig"""
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql://shruti:shruti@localhost/flaskblog'
+    SECRET_KEY = 'b240ac48e41e84a4278d195092289a8bdb08556b22f6760d'
+    SQLALCHEMY_TRACK_MODIFICATIONS =False
