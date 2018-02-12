fa7be14ae77a3fd282018fe8b05c1f9f6e0a53be
shrutih-cuelogic <shruti.hiremath@cuelogic.co.in>
2018-02-12 19:12:26 +0530

    added postgres port

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Auth:
 
 class Config(object):
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@0.0.0.0/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@localhost:5432/postgres'
     # Use this for docker running
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@flaskapp-postgres:5432/postgres'
     SECRET_KEY = 'b240ac48e41e84a4278d195092289a8bdb08556b22f6760d'
@@ -41,7 +41,7 @@ class Config(object):
 class TestConfig(Config):
     """docstring for TestConfig"""
     
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@0.0.0.0/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@localhost:5432/postgres'
     # Use this for docker running
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin123@flaskapp-postgres:5432/postgres'
     SECRET_KEY = 'b240ac48e41e84a4278d195092289a8bdb08556b22f6760d'
